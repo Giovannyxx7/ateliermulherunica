a64ffebe8a08d942135c9de8a6b3810039bd6a89
Giovannyxx7 <97688632+Giovannyxx7@users.noreply.github.com>
2022-04-25 12:07:45 -0300

    commit inicial

---
+++ Priscilla/js/script.js
@@ -0,0 +1,39 @@
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+    if (toggle && nav){
+
+        toggle.addEventListener('click',()=>{
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav')
+
+/*------------- Remover menu mobile--------------*/
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click',linkAction))
+
+/*------------- Deixa o link clicado com a class active-----*/
+const linkColor = document.querySelectorAll('nav_link')
+function colorLink(){
+
+    if(linkColor){
+        linkColor.forEach(L => L.classList.remove('active-link'))
+        this.classList.add('active-link')
+    }
+}
+linkColor.forEach(L=> L.addEventListener('click',colorLink))
+
+
+
+
+
+
+
+
